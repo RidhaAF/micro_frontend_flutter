9108ad53aca47f4d9f6dd768bf6279996b913c20
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2024-01-16 23:59:33 +0700

    refactor: update app title in MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Micro Frontend Flutter',
+      title: 'Micro Frontend Flutter App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
